db1919110916407d0e342628ee2b34d0079b8190
Quantum1128 <104086744+Quantum1128@users.noreply.github.com>
2023-07-29 23:44:30 -0700

    Add files via upload

---
+++ printer.py
@@ -0,0 +1,50 @@
+import serial
+import time
+from datetime import datetime
+
+def command(ser, command):
+  start_time = datetime.now() 
+  ser.write(str.encode(command)) 
+  while True:
+    line = str(ser.readline().decode()).replace("\r", "").encode() 
+    print(line)
+    if line == b'ok\n':
+      break  
+
+def defineP(pN, bR): 
+  g = serial.Serial(pN, int(bR))  
+  return g 
+
+def convert(fileIn, fileOut): 
+  final = ';converted' 
+  with open(fileIn, 'r') as g:  
+      M107 = 0
+      content = g.read().splitlines()  
+      for x in range(len(content)):  
+          if content[x] == 'M105': 
+              content[x] = 'M155'
+          if content[x] != '' and content[x] != ' ' and content[x][0] != ';': 
+              final = final + content[x] + r'\r\n' + '\r\n' 
+          if 'M107' in content[x] and M107 == 0: 
+              M107 = 1  
+          elif 'M107' in content[x] and M107 == 1: 
+              final = final + 'G0 X111 Y150 Z200' + r'\r\n' + '\r\n'  
+              final = final + 'G0 Y220 Z200' + r'\r\n' + '\r\n' 
+              final = final + 'G0 Z2' + r'\r\n' + '\r\n' 
+              final = final + 'G28' + r'\r\n' + '\r\n'
+  with open(fileOut, 'w') as h: 
+      h.write(final)  
+      print('Done!') 
+  return fileOut  
+
+def  sendFile(ser, file): 
+   with open(file, 'r') as d: 
+    content = d.read().splitlines()
+    for i in range(len(content)): 
+       content[i] = content[i].replace('\\r\\n', '\r\n') 
+    for x in range(len(content)):  
+      print(content[x]) 
+      command(ser, content[x]) 
+
+  
+ 
